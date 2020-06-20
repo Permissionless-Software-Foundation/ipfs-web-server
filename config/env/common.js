c27fdfd712f98d79c687b81a6c42f3f89ae50650
Chris Troutner <chris.troutner@gmail.com>
2020-06-20 16:17:11 -0700

    Customizing for wallet

--- config/env/common.js
+++ config/env/common.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   port: process.env.PORT || 3210,
-  BCHADDR: `bitcoincash:qr7u857krgsvq0dwe8rzlt5rcx35r6hnmu6glavtx0`,
+  BCHADDR: `bitcoincash:qq8mk8etntclfdkny2aknh4ylc0uaewalszh5eytnr`,
   // BCHADDR: `bitcoincash:qppngav5s88devt4ypv3vhgj643q06tctcx8fnzewp`, // troutsblog.com
   stateFileName: `state.json`,
   ipfsPort1: 4002,
